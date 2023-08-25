276175fb8ae008f5b535bafe05a76457fb98dafa
+ <+>
2023-08-25 15:21:57 +0300

    add app.js home.js

---
+++ src/Sharon.js
@@ -0,0 +1,35 @@
+import {Link} from 'react-router-dom'
+function Sharon(){
+    return <div>
+      <nav className="nav-links">
+          <Link className="blue">Projects</Link>
+          <Link className="blue">About</Link>
+          <Link className="blue">Contacts</Link>
+          </nav>
+    
+    <div className="sharon">
+      <h1>Sharon Asaja</h1>
+      <p>An upcoming Software Delevoper for frontend  and Backend web</p>
+    </div>
+    <div className="Aboutme">
+        <h2>Introduction</h2>
+          <p>A forensic scientist who aims at incooperating Software skills gained at Moringa school to achieve my goals
+          knowledge gained so far includes:
+            <br/>
+            <ul>
+              <li>
+                HTML
+                CSS
+                Javascript
+                React
+              </li>
+            </ul>
+            A passionate baker and cake lover.
+          </p>
+
+
+  </div>
+
+</div>
+}
+export default Sharon
