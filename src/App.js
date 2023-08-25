1abd4373ae21df643a5e5c7f9a39836b7f8c95df
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-08-25 16:22:07 +0300

    done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Mercy from './Mercy';
 import Home from './Home';
 import Benedict from './Benedict'
+import Sharon from './Sharon'
 import Mike from './Mike'
 import { Routes, Route,Link } from 'react-router-dom';
 
@@ -13,13 +14,16 @@ function App() {
       <div className='navbar'>
       <button style={{"marginLeft":"50%"}}><Link to="/mercy" style={{"color":'brown'}}>Mercy</Link></button>
       <button style={{"marginLeft":"50%"}}><Link to="/benedict" style={{"color":'brown'}}>Benedict</Link></button>
-      <button style={{"marginLeft":"50%"}}><Link to="/sharon" style={{"color":'brown'}}>Sharon</Link></button> 
+      <button style={{"marginLeft":"50%"}}><Link to="/sharon" style={{"color":'brown'}}>Sharon</Link></button>
+      <button style={{"marginLeft":"50%"}}><Link to="/mike" style={{"color":'brown'}}>Mike</Link></button>  
       </div>
       <Routes>
         <Route path ='/' element={<Home/>}/>
         <Route path ='/mercy' element={<Mercy/>}/>
         <Route path ='/benedict' element={<Benedict/>}/>
         <Route path ='/mike' element={<Mike/>}/>
+        <Route path ='/sharon' element={<Sharon/>}/>
+
       </Routes>
 
     
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,13 @@
 function Home(){
     return<div>
         <h1 style={{textAlign:"center"}}> This is Group 3</h1>
-        <p>Hello there,<br/>
+        {/* <p>Hello there,<br/>
            This is group 3, a group of FrontEnd Software Developers<br></br>
            If you are interested with our services,<br/>
            contact us through these email<br/>
            group3@gmail.com
 
-        </p>
+        </p> */}
 
 
     </div>
