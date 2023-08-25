44b0c0789e85108ec7a6a368348adbb785beb435
Mercychepngetich <138122032+Mercychepngetich@users.noreply.github.com>
2023-08-25 08:29:15 +0300

    adds App.js Home.js Mercy.js (#1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Mercy from './Mercy';
+import Home from './Home';
+import { Routes, Route,Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button style={{"marginLeft":"50%"}}><Link to="/mercy" style={{"color":'brown'}}>Mercy</Link></button> 
+      
+      <Routes>
+        <Route path ='/' element={<Home/>}/>
+        <Route path ='/mercy' element={<Mercy/>}/>
+      </Routes>
+
+    
     </div>
   );
 }
